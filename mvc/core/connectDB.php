9ac6a2181255e05a12eef904ffd0f8d2c09031b3
lelong1210 <lequanglong12102k2@gmail.com>
2021-09-20 09:55:43 +0700

    add all file

--- mvc/core/connectDB.php
+++ mvc/core/connectDB.php
@@ -17,26 +17,5 @@ class connectDB{
     function GetConn(){
         return $this->conn;
     }
-}/*9}Lj7N~9#ov?5~*{
-<?php
-class connectDB{
-    protected $servername = "localhost";
-    protected $username = "id17141465_root";
-    protected $password = "[Z)bNe56>[7oGfF*";
-    protected $dbname = "id17141465_testkienthuc";
-    protected $conn ; 
-    function __construct(){
-        try{
-            $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname",$this->username,$this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-           //echo "connect success";   
-        }catch(Exception $e){
-           // echo "fault => ".$e->getMessage();
-        }
-    }
-    function GetConn(){
-        return $this->conn;
-    }
-}
-?>*/ 
+}/*9}Lj7N~9#ov?5~*{*/
 ?>
