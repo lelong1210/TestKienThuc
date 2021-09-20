e09330f9c84e8accbde0f29011999a78bff8b667
lelong1210 <lequanglong12102k2@gmail.com>
2021-09-20 15:22:44 +0700

    add all file

--- mvc/controllers/product.php
+++ mvc/controllers/product.php
@@ -8,7 +8,7 @@ class product extends controller
     }
     function __call($method,$params){
         if($method == "dt" && isset($params[0])){
-            echo "co nhieu cai truyen";
+            echo "co nhieu cai truyen ===>".count($params);
         }else{
             echo "truyen k tham so";
         }
