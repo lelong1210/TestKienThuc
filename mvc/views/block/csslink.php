5bd8d3db35e807956c0fe16140ff7d8dd34a63ae
lelong1210 <lequanglong12102k2@gmail.com>
2021-09-22 08:38:11 +0700

    add all file

--- mvc/views/block/csslink.php
+++ mvc/views/block/csslink.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="./public/icon/fontawesome-free-5.15.4-web/css/all.min.css">
-<link rel="stylesheet" href="./public/css/style.css">
+<!-- <link rel="stylesheet" href="./public/icon/fontawesome-free-5.15.4-web/css/all.min.css">
+<link rel="stylesheet" href="./public/css/style.css"> -->
 <!-- 2 thu muc -->
-<link rel="stylesheet" href="../public/icon/fontawesome-free-5.15.4-web/css/all.min.css">
-<link rel="stylesheet" href="../public/css/style.css">
+<link rel="stylesheet" href="https://lequanglong12102k2.000webhostapp.com/public/icon/fontawesome-free-5.15.4-web/css/all.min.css">
+<link rel="stylesheet" href="https://lequanglong12102k2.000webhostapp.com/public/css/style.css">
