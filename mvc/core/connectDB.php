bb7235a8bb2b0571b628267d7f704e0f6ff5371c
lelong1210 <lequanglong12102k2@gmail.com>
2021-09-27 17:04:46 +0700

    add all file

--- mvc/core/connectDB.php
+++ mvc/core/connectDB.php
@@ -1,43 +1,9 @@
 <?php
-// class connectDB{
-//     protected $servername = "localhost";
-//     protected $username = "root";
-//     protected $password = "";
-//     protected $dbname = "testkienthuc";
-//     protected $conn ; 
-//     function __construct(){
-//         try{
-//             $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname",$this->username,$this->password);
-//             $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-//            //echo "connect success";   
-//         }catch(Exception $e){
-//            // echo "fault => ".$e->getMessage();
-//         }
-//     }
-//     // function connecDB(){
-//     //     try{
-//     //         $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname",$this->username,$this->password);
-//     //         $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-//     //        //echo "connect success";   
-//     //     }catch(Exception $e){
-//     //        // echo "fault => ".$e->getMessage();
-//     //     }        
-//     // }
-//     function GetConn(){
-//         return $this->conn;
-//     }
-// }/*
-//     id17141465_root
-//     9}Lj7N~9#ov?5~*{
-// */
-//     /*khong thich day*/
-?>
-<?php
 class connectDB{
     protected $servername = "localhost";
-    protected $username = "id17141465_root";
-    protected $password = "9}Lj7N~9#ov?5~*{";
-    protected $dbname = "id17141465_testkienthuc";
+    protected $username = "root";
+    protected $password = "";
+    protected $dbname = "testkienthuc";
     protected $conn ; 
     function __construct(){
         try{
@@ -48,8 +14,42 @@ class connectDB{
            // echo "fault => ".$e->getMessage();
         }
     }
+    // function connecDB(){
+    //     try{
+    //         $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname",$this->username,$this->password);
+    //         $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    //        //echo "connect success";   
+    //     }catch(Exception $e){
+    //        // echo "fault => ".$e->getMessage();
+    //     }        
+    // }
     function GetConn(){
         return $this->conn;
     }
-}
+}/*
+    id17141465_root
+    9}Lj7N~9#ov?5~*{
+*/
+    /*khong thich day*/
+?>
+<?php
+// class connectDB{
+//     protected $servername = "localhost";
+//     protected $username = "id17141465_root";
+//     protected $password = "9}Lj7N~9#ov?5~*{";
+//     protected $dbname = "id17141465_testkienthuc";
+//     protected $conn ; 
+//     function __construct(){
+//         try{
+//             $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname",$this->username,$this->password);
+//             $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+//            //echo "connect success";   
+//         }catch(Exception $e){
+//            // echo "fault => ".$e->getMessage();
+//         }
+//     }
+//     function GetConn(){
+//         return $this->conn;
+//     }
+// }
 ?>
